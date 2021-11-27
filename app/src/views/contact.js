7c372b0f226e0e94f8a46371e7da25a9889fc23e
TorrezMN <torrez.mn@gmail.com>
2021-11-26 21:38:42 -0300

    Agregado iconos de contacto.

--- app/src/views/contact.js
+++ app/src/views/contact.js
@@ -1,11 +1,38 @@
 import './static/css/contact.css';
-
+import { AiFillFacebook, 
+         AiFillTwitterCircle, 
+        AiOutlineLinkedin,
+        AiOutlineMail} from "react-icons/ai";
 
 
 function Contact(){
   return(
-    <div id='contact'>
-      <h3>Contact</h3>
+    <div id='socials'>
+      <div className='social_section'>
+        <div className='social_item'>
+            <a href='#'>
+              <AiFillFacebook/>
+            </a>
+        </div>
+        <div className='social_item'>
+            <a href='#'>
+              <AiFillTwitterCircle/>
+            </a>
+        </div>
+        <div className='social_item'>
+            <a href = '#'>
+              <AiOutlineLinkedin/>
+            </a>
+        </div>
+        <div className='social_item'>
+            <a href='#'>
+              <AiOutlineMail/>
+            </a>
+        </div>
+      </div>
+      <div className='social_section'></div>
+      <div className='social_section'></div>
+    
     </div>
   )
 }
